21d5e56b193e693898307c822730d66c73dd9f1b
Yang Zhao <jszyyx@163.com>
2020-02-12 15:19:51 +0800

    add config

--- stcript/crust-client.sh
+++ stcript/crust-client.sh
@@ -11,31 +11,40 @@ function help()
 {
 cat << EOF
 Usage:
-    -h,help      show help information
-    -v,version   show crust-client version
+    help      show help information
+    version   show crust-client version
+    config    show configuration files address
 EOF
 }
 
 function version()
 {
-    echo "crust-client version:\n\t"
+    echo "crust-client version:"
     cat $crust_client_main_install_dir/VERSION
-    echo "crust-chain version:\n\t"
+    echo "crust-chain version:"
     cat $crust_chain_main_install_dir/VERSION
-    echo "crust-api version:\n\t"
+    echo "crust-api version:"
     cat $crust_api_main_install_dir/VERSION
-    echo "crust-tee version:\n\t"
+    echo "crust-tee version:"
     cat $crust_tee_main_install_dir/VERSION
+}
+
+function config()
+{
+    echo "crust-tee configuration file address: $crust_tee_main_install_dir/etc/Config.json"
 }
 
 ############### MAIN BODY ###############
 
 # Command line
 case "$1" in
-    -v|version)
+    config)
+        config
+        ;;
+    version)
         version
         ;;
-    -h|help)
+    help)
         help
         ;;
     *)
