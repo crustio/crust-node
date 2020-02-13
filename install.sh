70d3028a25c92b9e8f596ab37ff11dbaafb29bfc
LowEntropyBody <jszyyx@163.com>
2020-02-13 15:16:47 +0800

    Optimize install workflow and add some clis (#2)
    * Add version function
    
    * fix version
    
    * add line into version
    
    * finish version function
    
    * add config
    
    * optimize cli
    
    * add crust chain spec

--- install.sh
+++ install.sh
@@ -7,10 +7,11 @@ crust_main_install_dir="/opt/crust"
 crust_chain_main_install_dir="$crust_main_install_dir/crust"
 crust_tee_main_install_dir="$crust_main_install_dir/crust-tee"
 crust_api_main_install_dir="$crust_main_install_dir/crust-api"
+crust_client_main_install_dir="$crust_main_install_dir/crust-client"
 
 crust_resource_dir="resource"
-crust_bin="$crust_resource_dir/crust"
-crust_subkey="$crust_resource_dir/subkey"
+crust_chain_package="$crust_resource_dir/crust.tar"
+crust_chain_resource_dir="$crust_resource_dir/crust"
 crust_api_package="$crust_resource_dir/crust-api.tar"
 crust_api_resource_dir="$crust_resource_dir/crust-api"
 crust_tee_package="$crust_resource_dir/crust-tee.tar"
@@ -39,13 +40,8 @@ if [ ! -d "$crust_resource_dir" ]; then
   exit 1
 fi
 
-if [ ! -f "$crust_bin" ]; then
-  verbose ERROR "Crust bin dosen't exist!"
-  exit 1
-fi
-
-if [ ! -f "$crust_subkey" ]; then
-  verbose ERROR "Crust subkey dosen't exist!"
+if [ ! -f "$crust_chain_package" ]; then
+  verbose ERROR "Crust chain package dosen't exist!"
   exit 1
 fi
 
@@ -68,28 +64,30 @@ fi
 
 verbose INFO "Unzip crust TEE package" h
 tar -xvf "$crust_tee_package" -C "$crust_resource_dir/" &>/dev/null
-verbose INFO " SUCCESS" t
-# ./$crust_tee_resource_dir/install.sh
+verbose INFO " SUCCESS\n" t
+#./$crust_tee_resource_dir/install.sh
 rm -rf $crust_tee_resource_dir
 
 # Install crust chain
 verbose INFO "---------- Installing crust chain ----------" n
-if [ ! -d "$crust_chain_main_install_dir" ]; then
-  verbose INFO "Create $crust_chain_main_install_dir folder" h
-  mkdir $crust_chain_main_install_dir
-  verbose INFO " SUCCESS" t
+if [ -d "$crust_chain_resource_dir" ]; then
+  rm -rf $crust_chain_resource_dir
 fi
 
-if [ ! -d "$crust_chain_main_install_dir/bin" ]; then
-  verbose INFO "Create $crust_chain_main_install_dir/bin folder" h
-  mkdir $crust_chain_main_install_dir/bin
+verbose INFO "Unzip crust chain package" h
+tar -xvf "$crust_chain_package" -C "$crust_resource_dir/" &>/dev/null
+verbose INFO " SUCCESS" t
+
+if [ -d "$crust_chain_main_install_dir" ]; then
+  verbose INFO "Uninstall old crust chain" h
+  rm -rf $crust_chain_main_install_dir
   verbose INFO " SUCCESS" t
 fi
 
 verbose INFO "Move crust chain to aim folder: $crust_chain_main_install_dir" h
-cp "$crust_bin" "$crust_chain_main_install_dir/bin/"
-cp "$crust_subkey" "$crust_chain_main_install_dir/bin/"
+cp -r $crust_chain_resource_dir $crust_main_install_dir
 verbose INFO " SUCCESS\n" t
+rm -rf $crust_chain_resource_dir
 
 # Install crust API
 verbose INFO "---------- Installing crust API ----------" n
@@ -114,7 +112,24 @@ rm -rf $crust_api_resource_dir
 
 # Install crust client
 verbose INFO "---------- Installing crust client ----------" n
-verbose INFO "Move crust client to /usr/bin" h
+if [ -d "$crust_client_main_install_dir" ]; then
+  verbose INFO "Uninstall old crust client" h
+  rm -rf $crust_client_main_install_dir
+  if [ -f "$crust_client_aim" ]; then
+    rm $crust_client_aim
+  fi
+  verbose INFO " SUCCESS" t
+fi
+
+verbose INFO "Create client directory $crust_client_main_install_dir" h
+mkdir $crust_client_main_install_dir
+verbose INFO " SUCCESS" t
+
+verbose INFO "Move crust-client files to aim folder: $crust_client_main_install_dir" h
+cp VERSION $crust_client_main_install_dir
+cp -r etc/ $crust_client_main_install_dir
+verbose INFO " SUCCESS" t
+
+verbose INFO "Move crust-client runnable stcript to /usr/bin" h
 cp $crust_client_sh $crust_client_aim
 verbose INFO " SUCCESS\n" t
-
--- stcript/crust-client.sh
+++ stcript/crust-client.sh
@@ -1,3 +1,55 @@
 #!/bin/bash
 
-echo "This is crust client!"
+# Some configuration
+crust_main_install_dir="/opt/crust"
+crust_chain_main_install_dir="$crust_main_install_dir/crust"
+crust_tee_main_install_dir="$crust_main_install_dir/crust-tee"
+crust_api_main_install_dir="$crust_main_install_dir/crust-api"
+crust_client_main_install_dir="$crust_main_install_dir/crust-client"
+
+function help()
+{
+cat << EOF
+Usage:
+    --help      show help information
+    --version   show crust-client version
+    --config    show configuration files address
+EOF
+}
+
+function version()
+{
+    echo "crust-client version:"
+    cat $crust_client_main_install_dir/VERSION
+    echo "crust-chain version:"
+    cat $crust_chain_main_install_dir/VERSION
+    echo "crust-api version:"
+    cat $crust_api_main_install_dir/VERSION
+    echo "crust-tee version:"
+    cat $crust_tee_main_install_dir/VERSION
+}
+
+function config()
+{
+    echo "crust-tee configuration file address: $crust_tee_main_install_dir/etc/Config.json"
+}
+
+############### MAIN BODY ###############
+
+# Command line
+case "$1" in
+    --config)
+        config
+        ;;
+    --version)
+        version
+        ;;
+    --help)
+        help
+        ;;
+    *)
+        help
+        exit 1
+        ;;
+esac
+exit 0
