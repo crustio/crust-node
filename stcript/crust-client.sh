3f7fa082d41bb8776b8b73b6cb03695f0a14fa5a
LowEntropyBody <jszyyx@163.com>
2020-02-15 13:39:28 +0800

    Add lanuch TEE, lanuch API and lanuch IPFS base command line (#4)
    * add tee-start-config
    
    * change config name
    
    * add api lanuch
    
    * add ipfs lanuch
    
    * add some todos

--- stcript/crust-client.sh
+++ stcript/crust-client.sh
@@ -13,10 +13,12 @@ function help()
 {
 cat << EOF
 Usage:
-    help      show help information
-    version   show crust-client version
-    config    show configuration files address
-    chain-lanuch-genesis <chain-start.config> <chain-identity-file>
+    help                                                              show help information
+    version                                                           show crust-client version
+    chain-lanuch-genesis <chain-lanuch.config> <chain-identity-file>  lanuch crust-chain as genesis node
+    api-lanuch <api-lanuch.config>                                    lanuch crust-api
+    ipfs-lanuch                                                       lanuch ipfs (cannot be customized for now, ipfs will be install in ~.ipfs/)      
+    tee-lanuch <tee-lanuch.json>                                      lanuch crust-tee (if you set api_base_url==validator_api_base_url in config file, you need to be genesis node)
 EOF
 }
 
@@ -30,11 +32,6 @@ function version()
     cat $crust_api_main_install_dir/VERSION
     echo "crust-tee version:"
     cat $crust_tee_main_install_dir/VERSION
-}
-
-function config()
-{
-    echo "crust-tee configuration file address: $crust_tee_main_install_dir/etc/Config.json"
 }
 
 function send_grandpa_key()
@@ -71,14 +68,14 @@ curl http://localhost:$1 -H "Content-Type:application/json;charset=utf-8" -d \
 
 function chainLanuchGenesis()
 {
-    verbose INFO "Check <chain-start.config> and <chain-identity-file>" h
+    verbose INFO "Check <chain-lanuch.config> and <chain-identity-file>" h
     if [ x"$1" = x"" ] || [ x"$2" = x"" ]; then
         help
         exit 1
     fi
 
     if [ ! -f "$1" ]; then
-        verbose ERROR "Can't find chain-start.config!"
+        verbose ERROR "Can't find chain-lanuch.config!"
         exit 1
     fi
     if [ ! -f "$2" ]; then
@@ -94,19 +91,19 @@ function chainLanuchGenesis()
 
     source $1
     if [ x"$base_path" = x"" ] || [ x"$port" = x"" ] || [ x"$ws_port" = x"" ] || [ x"$rpc_port" = x"" ] || [ x"$name" = x"" ]; then
-        verbose ERROR "Please give right chain-start.config!"
+        verbose ERROR "Please give right chain-lanuch.config!"
         exit 1
     fi
     chain_start_stcript="/opt/crust/crust/bin/crust --base-path $base_path --chain /opt/crust/crust-client/etc/crust_chain_spec_raw.json --port $port --ws-port $ws_port --rpc-port $rpc_port --telemetry-url ws://telemetry.polkadot.io:1024 --validator --name $name"
     verbose INFO " SUCCESS" t
     
-    verbose INFO "Try to kill old crust chain with same <chain_start_stcript>" h
+    verbose INFO "Try to kill old crust chain with same <chain-lanuch.json>" h
     crust_chain_pid=$(ps -ef | grep "$chain_start_stcript" | grep -v grep | awk '{print $2}')
     if [ x"$crust_chain_pid" != x"" ]; then
         kill -9 $crust_chain_pid
         if [ $? -ne 0 ]; then
             # If failed by using current user, kill it using root
-            execWithExpect "kill -9 $crust_chain_pid"
+            sudo "kill -9 $crust_chain_pid"
         fi
     fi
     verbose INFO " SUCCESS" t
@@ -137,22 +134,74 @@ function chainLanuchGenesis()
     send_babe_key $rpc_port $public_key_sr25519 $secret_phrase 
     verbose INFO " SUCCESS" t
 
-    verbose INFO "Try to kill old crust chain with same <chain_start_stcript> again" h
+    verbose INFO "Try to kill old crust chain with same <chain-lanuch.json> again" h
     crust_chain_pid=$(ps -ef | grep "$chain_start_stcript" | grep -v grep | awk '{print $2}')
     echo $crust_chain_pid
     if [ x"$crust_chain_pid" != x"" ]; then
         kill -9 $crust_chain_pid
         if [ $? -ne 0 ]; then
             # If failed by using current user, kill it using root
-            execWithExpect "kill -9 $crust_chain_pid"
+            sudo "kill -9 $crust_chain_pid"
         fi
     fi
     verbose INFO " SUCCESS" t
     rm $rand_log_file
 
-    verbose INFO "Lanuch crust chain <chain_start_stcript>" n
+    verbose INFO "Lanuch crust chain with <chain-lanuch.json>" n
     sleep 2
     eval $chain_start_stcript
+}
+
+ipfsLanuch()
+{
+    # TODO: Custom ipfs
+    verbose INFO "Lanuch ipfs" n
+    $crust_tee_main_install_dir/bin/ipfs daemon
+}
+
+apiLanuch()
+{
+    trap '{ cd - ; }' INT
+    verbose INFO "Check <api-lanuch.json>" h
+    if [ x"$1" = x"" ]; then
+        help
+        exit 1
+    fi
+
+    if [ ! -f "$1" ]; then
+        verbose ERROR "Can't find api-lanuch.json!"
+        exit 1
+    fi
+    source $1
+    verbose INFO " SUCCESS" t
+
+    verbose INFO "Lanuch crust API with <api-lanuch.json>" n
+    cd $crust_api_main_install_dir
+    CRUST_API_PORT=$crust_api_port CRUST_CHAIN_ENDPOINT=$crust_chain_endpoint yarn start
+}
+
+teeLanuch()
+{
+    verbose INFO "Check <tee-lanuch.json>" h
+    if [ x"$1" = x"" ]; then
+        help
+        exit 1
+    fi
+
+    if [ ! -f "$1" ]; then
+        verbose ERROR "Can't find tee-lanuch.json!"
+        exit 1
+    fi
+    verbose INFO " SUCCESS" t
+
+    # TODO: Analyze tee configurations
+    # tee_config=$(cat $1)
+    # api_base_url=$(getJsonValuesByAwk "$tee_config" "api_base_url" "null")
+    # validator_api_base_url=$(getJsonValuesByAwk "$tee_config" "validator_api_base_url" "null")
+    # echo api_base_url
+
+    verbose INFO "Lanuch crust TEE with <tee-lanuch.json>" n
+    $crust_tee_main_install_dir/bin/crust-tee -c $1
 }
 
 ############### MAIN BODY ###############
@@ -162,8 +211,14 @@ case "$1" in
     chain-lanuch-genesis)
         chainLanuchGenesis $2 $3
         ;;
-    config)
-        config
+    tee-lanuch)
+        teeLanuch $2
+        ;;
+    api-lanuch)
+        apiLanuch $2
+        ;;
+    ipfs-lanuch)
+        ipfsLanuch
         ;;
     version)
         version
--- stcript/utils.sh
+++ stcript/utils.sh
@@ -1,16 +1,50 @@
 #!/bin/bash
-function execWithExpect()
-{
-    local cmd=$1
-    local pkgPath=$2
-expect << EOF > $TMPFILE
-    set timeout $instTimeout
-    spawn sudo $cmd $pkgPath
-    expect "password"        { send "$passwd\n"  }
-    expect eof
-EOF
-    ! cat $TMPFILE | grep "error\|ERROR" &>/dev/null
-    return $?
+
+### 3 params: json, key, defaultValue
+function getJsonValuesByAwk() {
+    awk -v json="$1" -v key="$2" -v defaultValue="$3" 'BEGIN{
+        foundKeyCount = 0
+        while (length(json) > 0) {
+            # pos = index(json, "\""key"\""); ## 这行更快一些，但是如果有value是字符串，且刚好与要查找的key相同，会被误认为是key而导致值获取错误
+            pos = match(json, "\""key"\"[ \\t]*?:[ \\t]*");
+            if (pos == 0) {if (foundKeyCount == 0) {print defaultValue;} exit 0;}
+
+            ++foundKeyCount;
+            start = 0; stop = 0; layer = 0;
+            for (i = pos + length(key) + 1; i <= length(json); ++i) {
+                lastChar = substr(json, i - 1, 1)
+                currChar = substr(json, i, 1)
+
+                if (start <= 0) {
+                    if (lastChar == ":") {
+                        start = currChar == " " ? i + 1: i;
+                        if (currChar == "{" || currChar == "[") {
+                            layer = 1;
+                        }
+                    }
+                } else {
+                    if (currChar == "{" || currChar == "[") {
+                        ++layer;
+                    }
+                    if (currChar == "}" || currChar == "]") {
+                        --layer;
+                    }
+                    if ((currChar == "," || currChar == "}" || currChar == "]") && layer <= 0) {
+                        stop = currChar == "," ? i : i + 1 + layer;
+                        break;
+                    }
+                }
+            }
+
+            if (start <= 0 || stop <= 0 || start > length(json) || stop > length(json) || start >= stop) {
+                if (foundKeyCount == 0) {print defaultValue;} exit 0;
+            } else {
+                print substr(json, start, stop - start);
+            }
+
+            json = substr(json, stop + 1, length(json) - stop)
+        }
+    }'
 }
 
 function checkRes()
@@ -38,23 +72,6 @@ function checkRes()
     while [ -s $descriptor ]; do
         sleep 1
     done
-}
-
-function setTimeWait()
-{
-    local info=$1
-    local syncfile=$2
-    local index=1
-    local timeout=100
-    while [ ! -s "$syncfile" ] && [ $timeout -gt 0 ]; do
-        printf "%s\r" "${info}${index}s"
-        ((index++))
-        ((timeout--))
-        sleep 1
-    done
-
-    echo "${info}$(cat $SYNCFILE)"
-    true > $SYNCFILE
 }
 
 function verbose()
