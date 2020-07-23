ffa64baafa389054c49a5282b4480567fde7dd78
Yang Zhao <jszyyx@163.com>
2020-07-23 15:29:42 +0800

    mapping karst address in tee

--- generator/config-gen/tee-config.gen.js
+++ generator/config-gen/tee-config.gen.js
@@ -34,21 +34,27 @@ async function genTeeComposeConfig(config) {
   const srdVolumes = _.get(config, 'tee.srd_paths', [])
         .map((p) => `${p}:${p}`)
 
+  let tempVolumes = [
+    `${config.tee.base_path}:${config.tee.base_path}`,
+    ...srdVolumes,
+    './tee:/config'
+  ]
+
+  if (config.karst.base_path) { 
+    tempVolumes.push(`${config.karst.base_path}:${config.karst.base_path}`)
+  }
+
   return {
     image: 'crustio/crust-tee:0.5.0',
     network_mode: 'host',
     devices: [
       '/dev/isgx:/dev/isgx'
     ],
-    volumes: [
-      `${config.tee.base_path}:${config.tee.base_path}`,
-      ...srdVolumes,
-      './tee:/config'
-    ],
+    volumes: tempVolumes,
     environment: {
       ARGS: '-c /config/tee_config.json --debug',
     },
-    container_name: 'crust-tee:0.5.0',
+    container_name: 'crust-tee-0.5.0',
     restart: 'always',
   }
 }
