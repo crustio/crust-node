4757f720dcbb6df26bd79ef8fca10513d9c72e70
Yang Zhao <jszyyx@163.com>
2020-07-24 13:16:39 +0800

    give executable permissions

--- scripts/install_sgx_driver.sh
+++ scripts/install_sgx_driver.sh
@@ -3,7 +3,7 @@ scriptdir=$(cd `dirname $0`;pwd)
 basedir=$(cd $scriptdir/..;pwd)
 
 is_16=`cat /etc/issue | grep 16.04`
-if [ -z $is_16 ]; then
+if [ x"$is_16" = x"" ]; then
     driverbin=sgx_linux_x64_driver_2.6.0_4f5bb63.bin
     driverurl=https://download.01.org/intel-sgx/sgx-linux/2.7.1/distro/ubuntu18.04-server/$driverbin
 else
