9521f53a0d568c07ff3448edc280ad2b186bbce8
Yang Zhao <jszyyx@163.com>
2020-10-19 15:23:26 +0800

    put checks into utils

--- scripts/crust.sh
+++ scripts/crust.sh
@@ -521,28 +521,6 @@ Usage:
 EOF
 }
 
-check_port() {
-	port=$1
-	grep_port=`netstat -tlpn | grep "\b$port\b"`
-	if [ -n "$grep_port" ]; then
-		echo "[ERROR] please make sure port $port is not occupied"
-		return 1
-	fi
-}
-
-## 0 for running, 2 for error, 1 for stop
-check_docker_status()
-{
-	local exist=`docker inspect --format '{{.State.Running}}' $1 2>/dev/null`
-	if [ x"${exist}" == x"true" ]; then
-		return 0
-	elif [ "${exist}" == "false" ]; then
-		return 2
-	else
-		return 1
-	fi
-}
-
 tools_help()
 {
 cat << EOF
