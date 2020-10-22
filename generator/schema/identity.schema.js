b652cff55477dd8b13316dea5999d7214c2637dd
LowEntropyBody <jszyyx@163.com>
2020-10-22 17:19:59 +0800

    Cancel the detection of all values of backup (#79)

--- generator/schema/identity.schema.js
+++ generator/schema/identity.schema.js
@@ -4,24 +4,12 @@ const { logger } = require('../logger')
 
 const backupSchema = Joi.object({
   address: Joi.string().required(),
-  encoded: Joi.string().required(),
-  encoding: Joi.object({
-    content: Joi.array().items(Joi.string()).min(1).required(),
-    type: [Joi.array().items(Joi.string()).required(), Joi.string().required()],
-    version: Joi.string().required(),
-  }).required(),
-  meta: Joi.object({
-    genesisHash: Joi.string().length(66).regex(/^0x[0-9a-f]+$/).allow(null),
-    name: Joi.string().required(),
-    tags: Joi.array().items(Joi.string()),
-    whenCreated: Joi.date().timestamp().raw(),
-  }),
 })
 
 const identitySchema = Joi.object({
   backup: Joi.string().custom((value, helpers) => {
     try {
-      const result = backupSchema.validate(JSON.parse(value))
+      const result = backupSchema.validate(JSON.parse(value), {allowUnknown: true})
       if (result.error) {
         return helpers.error(result.error)
       }
