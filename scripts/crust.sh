3eb811804b653d00bcd27b86d5a481132e138251
LowEntropyBody <jszyyx@163.com>
2020-12-23 13:51:41 +0800

    Change sworker configuration (#109)
    * change sworker config
    
    * remove backup req

--- scripts/crust.sh
+++ scripts/crust.sh
@@ -775,7 +775,7 @@ Total space: ${data_folder_info[1]}
 Used space: ${data_folder_info[2]}
 Avail space: ${data_folder_info[3]}
 
--->Files folder (for ipfs and srd)<--
+-->Files folder (for ipfs and data of sworker)<--
 Path: /opt/crust/data/files
 File system: ${file_folder_info[0]}
 Total space: ${file_folder_info[1]}
@@ -814,11 +814,6 @@ change_srd()
 		return 1
 	fi
 
-	if [ x"$1" == x"0" ]; then 
-		log_err "Srd change number can't be zero"
-		return 1
-	fi
-
 	local a_or_b=`cat $basedir/etc/sWorker.ab`
 	check_docker_status crust-sworker-$a_or_b
 	if [ $? -ne 0 ]; then
@@ -830,11 +825,6 @@ change_srd()
 		log_err "No sworker configuration file"
 		return 1
 	fi
-
-	local backup=`cat $builddir/sworker/sworker_config.json | jq .chain.backup`
-	backup=${backup//\\/}
-	backup=${backup%?}
-	backup=${backup:1}
 
 	local base_url=`cat $builddir/sworker/sworker_config.json | jq .base_url`
 	base_url=${base_url%?}
