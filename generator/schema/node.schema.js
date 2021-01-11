edfca2e5a59c6483010cc68b4a13d08920e73205
Yang Zhao <jszyyx@163.com>
2021-01-11 17:10:25 +0800

    fix error

--- generator/schema/node.schema.js
+++ generator/schema/node.schema.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 const nodeSchema = Joi.object({
   chain: Joi.string().valid('authority', 'full').required(),
   sworker: Joi.string().valid('enable', 'disable').required(),
-  smanager: Joi.string().valid('enable', 'disable').required(),
+  smanager: Joi.string().valid('disable', 'isolation', 'member').required(),
   ipfs: Joi.string().valid('enable', 'disable').required(),
 })
 
