a1961c0e0aaa276feb1bed7e214ff5317f30ddaf
Yang Zhao <jszyyx@163.com>
2021-02-19 13:43:04 +0800

    set ipfs restart

--- generator/config-gen/ipfs-config.gen.js
+++ generator/config-gen/ipfs-config.gen.js
@@ -18,6 +18,7 @@ async function genIpfsComposeConfig(config) {
   return {
     image: 'ipfs/go-ipfs:latest',
     network_mode: 'host',
+    restart: 'always',
     volumes: [
       '/opt/crust/data/files/ipfs:/data/ipfs'
     ],
@@ -25,7 +26,7 @@ async function genIpfsComposeConfig(config) {
     environment: {
       IPFS_PROFILE: 'badgerds',
     },
-    command: '/bin/sh -c "/usr/local/bin/start_ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/37773 && /usr/local/bin/start_ipfs config Datastore.StorageMax 20000GB && /usr/local/bin/start_ipfs bootstrap add /ip4/101.33.32.103/tcp/4001/p2p/12D3KooWEVFe1uGbgsDCgt9GV5sAC864RNPPDJLTnX9phoWHuV2d && /usr/local/bin/start_ipfs daemon --migrate=true"',
+    command: '/bin/sh -c "/usr/local/bin/start_ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/37773 && /usr/local/bin/start_ipfs config Datastore.StorageMax 50000GB && /usr/local/bin/start_ipfs bootstrap add /ip4/101.33.32.103/tcp/4001/p2p/12D3KooWEVFe1uGbgsDCgt9GV5sAC864RNPPDJLTnX9phoWHuV2d && /usr/local/bin/start_ipfs daemon --migrate=true"',
   }
 }
 
