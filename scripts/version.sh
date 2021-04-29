b388345667b7f6fcd2034299fd1d805273fb642f
LowEntropyBody <jszyyx@163.com>
2021-04-29 15:49:54 +0800

    Version control (#140)
    * more version info
    
    * typo
    
    * change permission
    
    * fix log error
    
    * fix error

---
+++ scripts/version.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+basedir=/opt/crust/crust-node
+scriptdir=$basedir/scripts
+builddir=$basedir/build
+node_version="v0.10.0"
+
+source $scriptdir/utils.sh
+
+version()
+{
+printf "Node version: ${node_version}\n"
+inner_sworker_version
+inner_docker_version
+}
+
+inner_sworker_version()
+{
+	local config_file=$builddir/sworker/sworker_config.json
+	if [ x"$config_file" = x"" ]; then
+		return
+	fi
+
+	sworker_base_url=`cat $config_file | jq .base_url`
+
+	if [ x"$sworker_base_url" = x"" ]; then
+		return
+	fi
+
+	sworker_base_url=`echo "$sworker_base_url" | sed -e 's/^"//' -e 's/"$//'`
+
+    local id_info=`curl --max-time 30 $sworker_base_url/enclave/id_info 2>/dev/null`
+	if [ x"$id_info" = x"" ]; then
+		return
+	fi
+    printf "SWorker version:\n${id_info}\n"
+}
+
+inner_docker_version()
+{
+    local chain_image=(`docker images | grep '^\b'crustio/crust'\b ' | grep 'latest'`)
+    chain_image=${chain_image[2]}
+
+    local sworker_image=(`docker images | grep '^\b'crustio/crust-sworker'\b ' | grep 'latest'`)
+    sworker_image=${sworker_image[2]}
+
+    local cgen_image=(`docker images | grep '^\b'crustio/config-generator'\b ' | grep 'latest'`)
+    cgen_image=${cgen_image[2]}
+
+    local ipfs_image=(`docker images | grep '^\b'ipfs/go-ipfs'\b ' | grep 'latest'`)
+    ipfs_image=${ipfs_image[2]}
+
+    local api_image=(`docker images | grep '^\b'crustio/crust-api'\b ' | grep 'latest'`)
+    api_image=${api_image[2]}
+
+    local smanager_image=(`docker images | grep '^\b'crustio/crust-smanager'\b ' | grep 'latest'`)
+    smanager_image=${smanager_image[2]}
+
+    printf "Docker images:\n"
+    printf "  Chain: ${chain_image}\n"
+    printf "  SWorker: ${sworker_image}\n"
+    printf "  C-gen: ${cgen_image}\n"
+    printf "  IPFS: ${ipfs_image}\n"
+    printf "  API: ${api_image}\n"
+    printf "  Smanager: ${smanager_image}\n"
+}
