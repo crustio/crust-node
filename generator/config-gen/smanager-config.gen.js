e9787280387936589253cc012e0785e43a440034
Yang Zhao <jszyyx@163.com>
2021-08-10 10:06:13 +0800

    change smanager config

--- generator/config-gen/smanager-config.gen.js
+++ generator/config-gen/smanager-config.gen.js
@@ -1,32 +1,52 @@
 async function genSmanagerConfig(config, outputCfg) {
     const smanagerConfig = {
-      chain_addr: config.api.ws,
-      ipfs_addr: 'http://127.0.0.1:5001',
-      sworker_addr: 'http://127.0.0.1:12222',
-      mode: config.node.smanager,
-      address: config.identity.backup.address,
+      chain: {
+        account: config.identity.backup.address,
+        endPoint: config.api.ws
+      },
+      sworker: {
+        endPoint: "http://127.0.0.1:12222"
+      },
+      ipfs: {
+        endPoint: "http://127.0.0.1:5001"
+      },
+      node: {
+        role: config.node.smanager
+      },
+      telemetry: {
+        endPoint: "http://124.156.129.198:27654"
+      },
+      dataDir: "/data",
+      scheduler: {
+        minSrdRatio: 70,
+        strategy: {
+          existedFilesWeight: 0,
+          newFilesWeight: 100
+        }
+      }
     }
+  
     return {
       config: smanagerConfig,
-      paths: [],
+      paths: [{
+        required: true,
+        path: '/opt/crust/data/smanager',
+      }],
     }
   }
   
   async function genSmanagerComposeConfig(config) {
-    const args = [
-      config.api.ws,
-      'http://127.0.0.1:5001',
-      'http://127.0.0.1:12222',
-      config.node.smanager,
-      config.identity.backup.address,
-    ].join(' ')
     return {
       image: 'crustio/crust-smanager:latest',
       network_mode: 'host',
-      restart: 'always',
+      restart: 'unless-stopped',
       environment: {
-        ARGS: args,
+        SMANAGER_CONFIG: "/config",
       },
+      volumes: [
+        './smanager:/config',
+        '/opt/crust/data/smanager:/data'
+      ],
       logging: {
         driver: "json-file",
         options: {
